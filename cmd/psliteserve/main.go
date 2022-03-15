32e7b1ad21f22efe9b7f6e7a1efcb30aa9c1d1bc
Sri Panyam <sri.panyam@gmail.com>
2022-03-14 23:13:52 -0700

    Moving defaults to config package

--- cmd/psliteserve/main.go
+++ cmd/psliteserve/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"flag"
 	"github.com/panyam/pslite/cli"
-	"github.com/panyam/pslite/utils"
+	"github.com/panyam/pslite/config"
 )
 
 var (
-	port        = flag.Int("port", utils.DefaultServerPort(), "Port on which gRPC server should listen TCP conn.")
+	port        = flag.Int("port", config.DefaultServerPort(), "Port on which gRPC server should listen TCP conn.")
 	topics_root = flag.String("topics_root", "~/.pslite", "Root location where all topics are created")
 )
 
